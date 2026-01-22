0eda06ae1c833a78ba153daecc7a9f4a331bcf7b
Cattomancer <agusbustossp@gmail.com>
2026-01-22 03:39:50 -0300

    Try

--- crates/gpui/src/gpui.rs
+++ crates/gpui/src/gpui.rs
@@ -1,9 +1,9 @@
+#![feature(round_char_boundary)]
 #![doc = include_str!("../README.md")]
 #![deny(missing_docs)]
 #![allow(clippy::type_complexity)] // Not useful, GPUI makes heavy use of callbacks
 #![allow(clippy::collapsible_else_if)] // False positives in platform specific code
 #![allow(unused_mut)] // False positives in platform specific code
-#![feature(round_char_boundary)]
 
 extern crate self as gpui;
 
@@ -57,19 +57,26 @@ pub mod _ownership_and_data_flow;
 
 /// Do not touch, here be dragons for use by gpui_macros and such.
 #[doc(hidden)]
-pub mod private {
-    pub use anyhow;
-    pub use inventory;
-    pub use schemars;
-    pub use serde;
-    pub use serde_json;
+pub mod private
+{
+    pub use {
+        anyhow,
+        inventory,
+        schemars,
+        serde,
+        serde_json,
+    };
 }
 
-mod seal {
+mod seal
+{
     /// A mechanism for restricting implementations of a trait to only those in GPUI.
     /// See: <https://predr.ag/blog/definitive-guide-to-sealed-traits-in-rust/>
     pub trait Sealed {}
 }
+
+use std::any::Any;
+use std::future::Future;
 
 pub use action::*;
 pub use anyhow::Result;
@@ -84,7 +91,14 @@ pub use elements::*;
 pub use executor::*;
 pub use geometry::*;
 pub use global::*;
-pub use gpui_macros::{register_action, test, AppContext, IntoElement, Render, VisualContext};
+pub use gpui_macros::{
+    register_action,
+    test,
+    AppContext,
+    IntoElement,
+    Render,
+    VisualContext,
+};
 pub use http_client;
 pub use input::*;
 pub use inspector::*;
@@ -95,29 +109,39 @@ pub use path_builder::*;
 pub use platform::*;
 pub use profiler::*;
 #[cfg(any(target_os = "windows", target_os = "linux"))]
-pub(crate) use queue::{PriorityQueueReceiver, PriorityQueueSender};
+pub(crate) use queue::{
+    PriorityQueueReceiver,
+    PriorityQueueSender,
+};
 pub use refineable::*;
 pub use scene::*;
 pub use shared_string::*;
 pub use shared_uri::*;
-use std::{any::Any, future::Future};
 pub use style::*;
 pub use styled::*;
 pub use subscription::*;
 pub use svg_renderer::*;
 pub(crate) use tab_stop::*;
 use taffy::TaffyLayoutEngine;
-pub use taffy::{AvailableSpace, LayoutId};
+pub use taffy::{
+    AvailableSpace,
+    LayoutId,
+};
 #[cfg(any(test, feature = "test-support"))]
 pub use test::*;
 pub use text_system::*;
-pub use util::{arc_cow::ArcCow, FutureExt, Timeout};
+pub use util::arc_cow::ArcCow;
+pub use util::{
+    FutureExt,
+    Timeout,
+};
 pub use view::*;
 pub use window::*;
 
 /// The context trait, allows the different contexts in GPUI to be used
 /// interchangeably for certain operations.
-pub trait AppContext {
+pub trait AppContext
+{
     /// Create a new entity in the app context.
     #[expect(
         clippy::wrong_self_convention,
@@ -149,18 +173,15 @@ pub trait AppContext {
 
     /// Update a entity in the app context.
     fn as_mut<'a, T>(&'a mut self, handle: &Entity<T>) -> GpuiBorrow<'a, T>
-    where
-        T: 'static;
+    where T: 'static;
 
     /// Read a entity from the app context.
     fn read_entity<T, R>(&self, handle: &Entity<T>, read: impl FnOnce(&T, &App) -> R) -> R
-    where
-        T: 'static;
+    where T: 'static;
 
     /// Update a window for the given handle.
     fn update_window<T, F>(&mut self, window: AnyWindowHandle, f: F) -> Result<T>
-    where
-        F: FnOnce(AnyView, &mut Window, &mut App) -> T;
+    where F: FnOnce(AnyView, &mut Window, &mut App) -> T;
 
     /// Read a window off of the application context.
     fn read_window<T, R>(
@@ -173,29 +194,27 @@ pub trait AppContext {
 
     /// Spawn a future on a background thread
     fn background_spawn<R>(&self, future: impl Future<Output = R> + Send + 'static) -> Task<R>
-    where
-        R: Send + 'static;
+    where R: Send + 'static;
 
     /// Read a global from this app context
     fn read_global<G, R>(&self, callback: impl FnOnce(&G, &App) -> R) -> R
-    where
-        G: Global;
+    where G: Global;
 }
 
 /// Returned by [Context::reserve_entity] to later be passed to [Context::insert_entity].
 /// Allows you to obtain the [EntityId] for a entity before it is created.
 pub struct Reservation<T>(pub(crate) Slot<T>);
 
-impl<T: 'static> Reservation<T> {
+impl<T: 'static> Reservation<T>
+{
     /// Returns the [EntityId] that will be associated with the entity once it is inserted.
-    pub fn entity_id(&self) -> EntityId {
-        self.0.entity_id()
-    }
+    pub fn entity_id(&self) -> EntityId { self.0.entity_id() }
 }
 
 /// This trait is used for the different visual contexts in GPUI that
 /// require a window to be present.
-pub trait VisualContext: AppContext {
+pub trait VisualContext: AppContext
+{
     /// The result type for window operations.
     type Result<T>;
 
@@ -225,8 +244,7 @@ pub trait VisualContext: AppContext {
 
     /// Focus a entity in the window, if it implements the [`Focusable`] trait.
     fn focus<V>(&mut self, entity: &Entity<V>) -> Self::Result<()>
-    where
-        V: Focusable;
+    where V: Focusable;
 }
 
 /// A trait for tying together the types of a GPUI entity and the events it can
@@ -235,31 +253,26 @@ pub trait EventEmitter<E: Any>: 'static {}
 
 /// A helper trait for auto-implementing certain methods on contexts that
 /// can be used interchangeably.
-pub trait BorrowAppContext {
+pub trait BorrowAppContext
+{
     /// Set a global value on the context.
     fn set_global<T: Global>(&mut self, global: T);
     /// Updates the global state of the given type.
     fn update_global<G, R>(&mut self, f: impl FnOnce(&mut G, &mut Self) -> R) -> R
-    where
-        G: Global;
+    where G: Global;
     /// Updates the global state of the given type, creating a default if it didn't exist before.
     fn update_default_global<G, R>(&mut self, f: impl FnOnce(&mut G, &mut Self) -> R) -> R
-    where
-        G: Global + Default;
+    where G: Global + Default;
 }
 
 impl<C> BorrowAppContext for C
-where
-    C: std::borrow::BorrowMut<App>,
+where C: std::borrow::BorrowMut<App>
 {
-    fn set_global<G: Global>(&mut self, global: G) {
-        self.borrow_mut().set_global(global)
-    }
+    fn set_global<G: Global>(&mut self, global: G) { self.borrow_mut().set_global(global) }
 
     #[track_caller]
     fn update_global<G, R>(&mut self, f: impl FnOnce(&mut G, &mut Self) -> R) -> R
-    where
-        G: Global,
+    where G: Global
     {
         let mut global = self.borrow_mut().lease_global::<G>();
         let result = f(&mut global, self);
@@ -268,8 +281,7 @@ where
     }
 
     fn update_default_global<G, R>(&mut self, f: impl FnOnce(&mut G, &mut Self) -> R) -> R
-    where
-        G: Global + Default,
+    where G: Global + Default
     {
         self.borrow_mut().default_global::<G>();
         self.update_global(f)
@@ -278,7 +290,8 @@ where
 
 /// Information about the GPU GPUI is running on.
 #[derive(Default, Debug, serde::Serialize, serde::Deserialize, Clone)]
-pub struct GpuSpecs {
+pub struct GpuSpecs
+{
     /// Whether the GPU is really a fake (like `llvmpipe`) running on the CPU.
     pub is_software_emulated: bool,
     /// The name of the device, as reported by Vulkan.
